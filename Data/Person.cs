afb0bc4eb87b6da37101dc91563edc0e59828172
NimishMahjan <81397525+NimishMahjan@users.noreply.github.com>
2024-09-25 14:31:46 -0400

    added ef core and azure aql database connection

---
+++ Data/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace azure_app_nim_git.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime DateOFBirth { get; set; }
+    }
+}
